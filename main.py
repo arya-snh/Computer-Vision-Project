125f1e54c64052fe8234e88b0cedbdf553554550
arya <arya20498@iiitd.ac.in>
2023-05-09 04:42:22 +0530

    update

--- main.py
+++ main.py
@@ -1,5 +1,71 @@
 import numpy as np
 import cv2
+import math
+import os
+from objloader_simple import *
+DEFAULT_COLOR = (0, 0, 0)
+
+def hex_to_rgb(hex_color):
+    """
+    Helper function to convert hex strings to RGB
+    """
+    hex_color = hex_color.lstrip('#')
+    h_len = len(hex_color)
+    return tuple(int(hex_color[i:i + h_len // 3], 16) for i in range(0, h_len, h_len // 3))
+
+def render(img, obj, projection, model, color=False):
+    """
+    Render a loaded obj model into the current video frame
+    """
+    vertices = obj.vertices
+    scale_matrix = np.eye(3) * 3
+    h, w = model.shape[0], model.shape[1]
+
+    for face in obj.faces:
+        face_vertices = face[0]
+        points = np.array([vertices[vertex - 1] for vertex in face_vertices])
+        points = np.dot(points, scale_matrix)
+        # render model in the middle of the reference surface. To do so,
+        # model points must be displaced
+        points = np.array([[p[0] + w / 2, p[1] + h / 2, p[2]] for p in points])
+        dst = cv2.perspectiveTransform(points.reshape(-1, 1, 3), projection)
+        imgpts = np.int32(dst)
+        if color is False:
+            cv2.fillConvexPoly(img, imgpts, DEFAULT_COLOR)
+        else:
+            color = hex_to_rgb(face[-1])
+            color = color[::-1]  # reverse
+            cv2.fillConvexPoly(img, imgpts, color)
+
+    return img
+
+def projection_matrix(camera_parameters, homography):
+    """
+    From the camera calibration matrix and the estimated homography
+    compute the 3D projection matrix
+    """
+    # Compute rotation along the x and y axis as well as the translation
+    homography = homography * (-1)
+    rot_and_transl = np.dot(np.linalg.inv(camera_parameters), homography)
+    col_1 = rot_and_transl[:, 0]
+    col_2 = rot_and_transl[:, 1]
+    col_3 = rot_and_transl[:, 2]
+    # normalise vectors
+    l = math.sqrt(np.linalg.norm(col_1, 2) * np.linalg.norm(col_2, 2))
+    rot_1 = col_1 / l
+    rot_2 = col_2 / l
+    translation = col_3 / l
+    # compute the orthonormal basis
+    c = rot_1 + rot_2
+    p = np.cross(rot_1, rot_2)
+    d = np.cross(c, p)
+    rot_1 = np.dot(c / np.linalg.norm(c, 2) + d / np.linalg.norm(d, 2), 1 / math.sqrt(2))
+    rot_2 = np.dot(c / np.linalg.norm(c, 2) - d / np.linalg.norm(d, 2), 1 / math.sqrt(2))
+    rot_3 = np.cross(rot_1, rot_2)
+    # finally, compute the 3D projection matrix from the model to the current frame
+    projection = np.stack((rot_1, rot_2, rot_3, translation)).T
+    return np.dot(camera_parameters, projection)
+
 
 cap = cv2.VideoCapture(0)
 
@@ -14,7 +80,7 @@ success, imgVideo = myVid.read()
 target_width = 400  # Set the desired width
 scale_percent = target_width / img1.shape[1]  # Calculate the scale percentage
 target_height = int(img1.shape[0] * scale_percent)  # Calculate the target height
-
+camera_parameters = np.array([[955.88, 0, 566.63], [0, 958.46, 360.43], [0, 0, 1]])
 img1 = cv2.resize(img1, (target_width, target_height))
 
 ht,wt,ct = img1.shape
@@ -29,10 +95,11 @@ img1 = cv2.drawKeypoints(img1,kp1,None)
 # cv2.imshow('Image 1',img1)
 # cv2.waitKey(0)
 total_count = myVid.get(cv2.CAP_PROP_FRAME_COUNT)
-while frameCounter <= total_count:
+obj = OBJ('fox.obj', swapyz=True) 
+while True:
 
-  img_tgt = cv2.imread('TargetImage.jpg')
-
+  # img_tgt = cv2.imread('TargetImage.jpg')
+  ret, img_tgt = cap.read()
   target_width = 400  # Set the desired width
   scale_percent = target_width / img_tgt.shape[1]  # Calculate the scale percentage
   target_height = int(img_tgt.shape[0] * scale_percent)  # Calculate the target height
@@ -64,7 +131,7 @@ while frameCounter <= total_count:
   print(len(good))
   imgFeatures = cv2.drawMatches(img1,kp1,img_tgt,kp2,good,None,flags=2)
 
-  if(len(good)>20):
+  if(len(good)>2):
     detection = True
     srcPts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1,1,2)
     dstpts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1,1,2)
@@ -82,13 +149,17 @@ while frameCounter <= total_count:
     img_aug = cv2.bitwise_and(img_aug, img_aug, mask = maskInv)
     img_aug = cv2.bitwise_or(imgWarp, img_aug )
 
-  cv2.imshow('img_aug',img_aug)
+  projection = projection_matrix(camera_parameters, matrix)  
+  # project cube or model
+  print(img1.shape)
+  frame = render(img2, obj, projection, img1, False)
+  cv2.imshow('frame',frame)
   # cv2.imshow('imgWarp',imgWarp)
   # cv2.imshow('img2',img2)
   # cv2.imshow('Image Features',imgFeatures)
   # cv2.imshow('Image 1',img1)
   # cv2.imshow('Image Video',imgVideo)
   # cv2.imshow('Image tgt',img_tgt)
-  cv2.waitKey(1)
+  cv2.waitKey(0)
   frameCounter+=1
   # break
